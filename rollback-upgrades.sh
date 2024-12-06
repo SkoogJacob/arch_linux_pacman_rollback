00f021a2e6a43120d4668a0988ed6374059e532d
Jacob <42621330+SkoogJacob@users.noreply.github.com>
2024-12-06 10:48:26 +0000

    Add script file

---
+++ rollback-upgrades.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+if [[ -z $1 ]]; then
+  echo "You must provide a date for the script"
+  exit 1
+fi
+if [[ "$1" = "-h" ]]; then
+  echo "This script undoes all pacman upgrades after a provided date. Provide a date up to 2 days ago as the first and only positional argument to this script"
+  exit 0
+fi
+
+current_month_year="$(date '+%Y-%m')"
+td="$(date '+%Y-%m-%d')"
+fd=$(date -d "$1" '+%Y-%m-%d')
+if [[ $? != 0 ]]; then
+  echo "failed to parse date"
+  exit 1
+fi
+
+function check_input_date_not_more_than_2_days_ago() {
+  local fd_seconds
+  local td_seconds
+  fd_seconds=$(date -d "$fd" +%s)
+  td_seconds=$(date +%s)
+  local diff=$((td_seconds - fd_seconds))
+  local two_days_in_seconds='172800'
+
+  if ((diff > two_days_in_seconds)); then
+    echo "This script blocks rolling back upgrades older than 2 days"
+    exit 1
+  fi
+}
+
+TEMPFILE="$(mktemp /tmp/rollback-XXXX)"
+trap 'rm -f $TEMPFILE' EXIT
+
+rg "$current_month_year" /var/log/pacman.log | rg upgraded >"$TEMPFILE"
+
+function date_to_timestamp() {
+  date -d "$1" +%s
+}
+
+function get_lines() {
+  # Loop through each line in the file
+  while IFS= read -r line; do
+    # Extract date from the line (assuming it's the only date in the line)
+    matched_date=$(echo "$line" | awk '{print $1}' | sed 's/\[//g' | sed 's/\]//g' | awk -F'T' '{print $1}')
+    # Convert the matched date to a timestamp
+
+    # Compare the matched date with the start date and current date
+    if [[ "$matched_date" > "$fd" && ("$matched_date" < "$td" || "$matched_date" = "$td") ]]; then
+      echo "$line" # Print the line if it matches the date range
+    fi
+  done <"$TEMPFILE"
+}
+lines="$(get_lines | awk '{print $4 $5}' | sed 's/(/-/' | sort | uniq)"
+echo "$lines" | xargs -I{} sh -c 'find /var/cache/pacman/pkg/ -name "*{}*.zst"' | xargs -I{} sh -c 'sudo pacman -U {} --noconfirm'
